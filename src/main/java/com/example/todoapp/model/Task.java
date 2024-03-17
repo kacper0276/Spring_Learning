43d824db746b8f8646998dccffc4359a872e5524
kacper0276 <kacper0276@op.pl>
2024-03-17 22:49:22 +0100

    Encja

--- src/main/java/com/example/todoapp/model/Task.java
+++ src/main/java/com/example/todoapp/model/Task.java
@@ -1,10 +1,22 @@
 package com.example.todoapp.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity // Oznacza tabelę w bazie która odpowiada takiej klasie
 @Table(name = "tasks")
+@Getter
+@Setter
 public class Task {
+    @Id
+    private int id;
 
+    @Column(name = "description")
+    private String description;
+
+    private boolean done;
 }
