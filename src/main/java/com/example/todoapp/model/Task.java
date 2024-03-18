9a689b9a09847c9a7d94381953e88da88826a990
kacper0276 <kacper0276@op.pl>
2024-03-18 13:51:43 +0100

    Pierwszy request do bazy danych, Dodanie @GeneratedValue (Auto_Increment)

--- src/main/java/com/example/todoapp/model/Task.java
+++ src/main/java/com/example/todoapp/model/Task.java
@@ -1,9 +1,6 @@
 package com.example.todoapp.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +10,7 @@ import lombok.Setter;
 @Setter
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "description")
