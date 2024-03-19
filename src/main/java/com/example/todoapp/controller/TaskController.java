9d6471f834aed3b300f338f8820d491be35d87f6
kacper0276 <kacper0276@op.pl>
2024-03-19 12:25:24 +0100

    @RequestMapping

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -5,10 +5,19 @@ import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RepositoryRestController
 @AllArgsConstructor
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository taskRepository;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    ResponseEntity<?> readAllTasks() {
+        logger.warn("Exposing all the tasks!");
+        return ResponseEntity.ok(taskRepository.findAll());
+    }
 }
