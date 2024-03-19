f8cd79ab28a7dc7624102471e768c10b9908105d
kacper0276 <kacper0276@op.pl>
2024-03-19 11:53:44 +0100

    HATEOAS

--- src/main/java/com/example/todoapp/model/TaskRepository.java
+++ src/main/java/com/example/todoapp/model/TaskRepository.java
@@ -3,6 +3,6 @@ package com.example.todoapp.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "todos")
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 }
