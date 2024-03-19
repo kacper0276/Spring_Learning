2fdb3f9c57b32fa541de8359f12fa7c1d781d437
kacper0276 <kacper0276@op.pl>
2024-03-19 21:52:47 +0100

    Czyszczenie kodu - Nowsza wersja

--- src/main/java/com/example/todoapp/model/TaskRepository.java
+++ src/main/java/com/example/todoapp/model/TaskRepository.java
@@ -1,13 +1,11 @@
 package com.example.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-    @RestResource(path = "done", rel = "done")
     List<Task> findByDoneIsTrue(); // LUB List<Task> findByDone(@Param("state") boolean done);
 }
