7629005838e1b3fc8720ef78a09956633394cff0
kacper0276 <kacper0276@op.pl>
2024-03-19 11:58:36 +0100

    Opcje adnotacji

--- src/main/java/com/example/todoapp/model/TaskRepository.java
+++ src/main/java/com/example/todoapp/model/TaskRepository.java
@@ -3,6 +3,6 @@ package com.example.todoapp.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "todos")
+@RepositoryRestResource // (path = "todos", collectionResourceRel = "todos") collectionResourceRel - jak powinien być opisywany w kolekcji
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 }
