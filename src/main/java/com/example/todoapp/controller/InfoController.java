a7f10d8bc3efe3ac3dd19befeead0a2cdd0f7d80
kacper0276 <kacper0276@op.pl>
2024-03-20 23:51:10 +0100

    @Autowired oraz DataSourceProperties - czyli inny sposób na pobieranie properties

--- src/main/java/com/example/todoapp/controller/InfoController.java
+++ src/main/java/com/example/todoapp/controller/InfoController.java
@@ -1,19 +1,21 @@
 package com.example.todoapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
-    @Value("${spring.datasource.url}")
-    private String url;
+    @Autowired
+    private DataSourceProperties dataSource;
     @Value("${my.prop}")
     private String myProp;
 
     @GetMapping("/info/url")
     String url() {
-        return url;
+        return dataSource.getUrl();
     }
 
     @GetMapping("/info/prop")
