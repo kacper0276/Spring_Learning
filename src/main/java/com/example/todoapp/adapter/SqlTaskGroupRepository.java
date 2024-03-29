b1437c1ff902a09e061b0b3bc24cdfc169b5c313
kacper0276 <kacper0276@op.pl>
2024-03-29 02:02:57 +0100

    HQL - Hibernate Query Language

--- src/main/java/com/example/todoapp/adapter/SqlTaskGroupRepository.java
+++ src/main/java/com/example/todoapp/adapter/SqlTaskGroupRepository.java
@@ -3,8 +3,14 @@ package com.example.todoapp.adapter;
 import com.example.todoapp.model.TaskGroup;
 import com.example.todoapp.model.TaskGroupRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
+    @Override
+    @Query("select * from TaskGroup g join fetch g.tasks") // NativeQuery = false - zapytanie na encjach, NativeQuery = true - zapytanie na tabelach bazodanowych
+    List<TaskGroup> findAll();
 }
