6c572590b4cf048a183226afa2ad3d77e0996fa0
kacper0276 <kacper0276@op.pl>
2024-03-30 13:40:42 +0100

    Utworzenie nowego servisu (ProjectService)

--- src/main/java/com/example/todoapp/TaskConfigurationProperties.java
+++ src/main/java/com/example/todoapp/TaskConfigurationProperties.java
@@ -1,16 +1,30 @@
 package com.example.todoapp;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties("task")
+@Configuration
 public class TaskConfigurationProperties {
-    private boolean allowMultipleTasksFromTemplate;
+    private Template template;
 
-    public boolean isAllowMultipleTasksFromTemplate() {
-        return allowMultipleTasksFromTemplate;
+    public Template getTemplate() {
+        return template;
     }
 
-    public void setAllowMultipleTasksFromTemplate(boolean allowMultipleTasksFromTemplate) {
-        this.allowMultipleTasksFromTemplate = allowMultipleTasksFromTemplate;
+    public void setTemplate(final Template template) {
+        this.template = template;
+    }
+
+    public static class Template {
+        private boolean allowMultipleTasks;
+
+        public boolean isAllowMultipleTasks() {
+            return allowMultipleTasks;
+        }
+
+        public void setAllowMultipleTasks(final boolean allowMultipleTasks) {
+            this.allowMultipleTasks = allowMultipleTasks;
+        }
     }
 }
---
+++ src/main/java/com/example/todoapp/logic/ProjectService.java
@@ -0,0 +1,47 @@
+package com.example.todoapp.logic;
+
+import com.example.todoapp.TaskConfigurationProperties;
+import com.example.todoapp.model.*;
+import com.example.todoapp.model.projection.GroupReadModel;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ProjectService {
+    private final ProjectRepository repository;
+    private final TaskGroupRepository taskGroupRepository;
+    private final TaskConfigurationProperties config;
+
+    public List<Project> readAll() {
+        return repository.findAll();
+    }
+
+    public Project save(final Project toSave) {
+        return repository.save(toSave);
+    }
+
+    public GroupReadModel createGroup(LocalDateTime deadline, int projectId) {
+        if(!config.getTemplate().isAllowMultipleTasks() && taskGroupRepository.existsByDoneIsFalseAndProject_Id(projectId)) {
+            throw new IllegalStateException("Only one undone group from project is allowed");
+        }
+
+        TaskGroup result = repository.findById(projectId)
+                .map(project -> {
+                   var targetGroup = new TaskGroup();
+                    targetGroup.setDescription(project.getDescription());
+                    targetGroup.setTasks(
+                           project.getSteps().stream()
+                           .map(step -> new Task(step.getDescription(), deadline.plusDays(step.getDaysToDeadline())))
+                           .collect(Collectors.toSet())
+                   );
+                   return targetGroup;
+                }).orElseThrow(() -> new IllegalArgumentException("Project with given id not found"));
+
+        return new GroupReadModel(result);
+    }
+}
