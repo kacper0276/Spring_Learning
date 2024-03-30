1c9b1dccf554b2ceae84dd249425cce4c0c9283e
kacper0276 <kacper0276@op.pl>
2024-03-30 22:11:42 +0100

    Testy jednostkowe - utworzenie

---
+++ src/test/java/com/example/todoapp/logic/ProjectServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.todoapp.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void createGroup() {
+    }
+}
