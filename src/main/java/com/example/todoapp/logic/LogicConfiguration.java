64bd56ce080845c5ef2875fe0969ddb45d5c2738
kacper0276 <kacper0276@op.pl>
2024-04-04 09:18:12 +0200

    Usunięcie pliku XML - poprawki kodu po tym

--- src/main/java/com/example/todoapp/logic/LogicConfiguration.java
+++ src/main/java/com/example/todoapp/logic/LogicConfiguration.java
@@ -5,13 +5,11 @@ import com.example.todoapp.model.ProjectRepository;
 import com.example.todoapp.model.TaskGroupRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource("classpath:applicationContext.xml") // classpath - resources
 public class LogicConfiguration {
-//    @Bean // Rejestracja Beanem ProjectService
-//    ProjectService service(ProjectRepository respository, TaskGroupRepository taskGroupRepository, TaskConfigurationProperties config) {
-//        return new ProjectService(respository, taskGroupRepository, config);
-//    }
+    @Bean // Rejestracja Beanem ProjectService
+    ProjectService service(ProjectRepository respository, TaskGroupRepository taskGroupRepository, TaskConfigurationProperties config) {
+        return new ProjectService(respository, taskGroupRepository, config);
+    }
 }
