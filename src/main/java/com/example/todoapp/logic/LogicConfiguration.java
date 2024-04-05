7d7a438c50a365f82ae193f9957a1dc0ed48e0fc
kacper0276 <kacper0276@op.pl>
2024-04-05 12:52:39 +0200

    Usunięcie @Service z TaskGroupService

--- src/main/java/com/example/todoapp/logic/LogicConfiguration.java
+++ src/main/java/com/example/todoapp/logic/LogicConfiguration.java
@@ -3,13 +3,19 @@ package com.example.todoapp.logic;
 import com.example.todoapp.TaskConfigurationProperties;
 import com.example.todoapp.model.ProjectRepository;
 import com.example.todoapp.model.TaskGroupRepository;
+import com.example.todoapp.model.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogicConfiguration {
     @Bean // Rejestracja Beanem ProjectService
-    ProjectService service(ProjectRepository respository, TaskGroupRepository taskGroupRepository, TaskConfigurationProperties config) {
+    ProjectService projectService(final ProjectRepository respository, final TaskGroupRepository taskGroupRepository, final TaskConfigurationProperties config) {
         return new ProjectService(respository, taskGroupRepository, config);
+    }
+
+    @Bean
+    TaskGroupService taskGroupService(final TaskGroupRepository taskGroupRepository, final TaskRepository taskRepository) {
+        return new TaskGroupService(taskGroupRepository, taskRepository);
     }
 }
