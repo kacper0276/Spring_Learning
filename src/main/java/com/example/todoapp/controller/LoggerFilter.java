ca22bab120395a46a9ac6056b9e17e3c0b8252d0
kacper0276 <kacper0276@op.pl>
2024-04-07 15:00:06 +0200

    Interfejs Ordered - czyli ustalanie priorytetu filtra

--- src/main/java/com/example/todoapp/controller/LoggerFilter.java
+++ src/main/java/com/example/todoapp/controller/LoggerFilter.java
@@ -1,14 +1,21 @@
 package com.example.todoapp.controller;
 
 import jakarta.servlet.*;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-public class LoggerFilter implements Filter {
+public class LoggerFilter implements Filter, Ordered {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
+    }
+
+    // Ustalanie priorytetu, jeśli nasz filtr ma return 1 a inny return 2 to wyzszy priorytet ma ten z 2
+    @Override
+    public int getOrder() {
+        return 2;
     }
 }
