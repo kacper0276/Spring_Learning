a8be5fbb5c85165ecd4ce64cbbfad8f0c3377f1b
kacper0276 <kacper0276@op.pl>
2024-04-07 09:10:50 +0200

    @RequestMapping - na kontrolerze

--- src/main/java/com/example/todoapp/controller/TaskController.java
+++ src/main/java/com/example/todoapp/controller/TaskController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 //@RepositoryRestController
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository taskRepository;
@@ -29,30 +30,32 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks", params = {"!sort", "!page", "!size"}) // Pewność że nie zostały wykorzystane te parametry
+    @RequestMapping(method = RequestMethod.GET, path = "/", params = {"!sort", "!page", "!size"}) // Pewność że nie zostały wykorzystane te parametry
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(taskRepository.findAll());
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks")
+    @RequestMapping(method = RequestMethod.GET, path = "/")
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(taskRepository.findAll(page).getContent()); // Jak bez .getContent() to zwraca informację o paginacji (ile el na strone, która strona i zamiast List<Task> zwraca Page<Task>
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     ResponseEntity<Optional<Task>> readTaskById(@PathVariable int id) {
         return ResponseEntity.ok(taskRepository.findById(id));
     }
 
-    @RequestMapping(method = RequestMethod.POST, path = "/tasks")
+    @GetMapping("/search/done")
+
+    @RequestMapping(method = RequestMethod.POST, path = "/")
     ResponseEntity<?> createTask(@RequestBody Task task) {
         logger.info("Create task");
         return ResponseEntity.ok(taskRepository.save(task));
     }
 
-    @RequestMapping(method = RequestMethod.PUT, path = "/tasks/{id}")
+    @RequestMapping(method = RequestMethod.PUT, path = "/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) { // zamiast @Param można użyć @PathVariable("id") int id
         taskRepository.findById(id)
                 .ifPresent(task -> {
@@ -63,7 +66,7 @@ public class TaskController {
     }
 
     @Transactional // Na początku transaction begin a na końcu transaction commit, jeżeli poleci exception to cała operacja się nie wykona, wszystkie zmiany wycofane, samo się zapisze, albo transactional albo repository.save() wszędzie
-    @PatchMapping(path = "/tasks/{id}")
+    @PatchMapping(path = "/{id}")
     ResponseEntity<?> toggleTask(@PathVariable int id) { // zamiast @Param można użyć @PathVariable("id") int id
         taskRepository.findById(id)
                 .ifPresent(task -> task.setDone(!task.isDone()));
