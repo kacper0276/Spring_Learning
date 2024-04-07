2807fab0c4d53e55cc6d4d659d6e817cc10ae8d7
kacper0276 <kacper0276@op.pl>
2024-04-07 15:01:53 +0200

    Zastąpienie Interfejs Ordered - @Order

--- src/main/java/com/example/todoapp/controller/LoggerFilter.java
+++ src/main/java/com/example/todoapp/controller/LoggerFilter.java
@@ -2,20 +2,17 @@ package com.example.todoapp.controller;
 
 import jakarta.servlet.*;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Order(Ordered.HIGHEST_PRECEDENCE) // To samo co implementacja Ordered, HIGHEST_PRECEDENCE - wykona się przed wszystkimi
 @Component
-public class LoggerFilter implements Filter, Ordered {
+public class LoggerFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
     }
 
-    // Ustalanie priorytetu, jeśli nasz filtr ma return 1 a inny return 2 to wyzszy priorytet ma ten z 2
-    @Override
-    public int getOrder() {
-        return 2;
-    }
 }
