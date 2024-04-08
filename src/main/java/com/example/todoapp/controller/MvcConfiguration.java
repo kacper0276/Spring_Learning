a795f8b0ee9ab175ec832359e9cf7f9095011fb2
kacper0276 <kacper0276@op.pl>
2024-04-08 12:41:39 +0200

    Wstrzyknięcie wszystkich interceptorów (wstrzyknięcie kolekcji)

--- src/main/java/com/example/todoapp/controller/MvcConfiguration.java
+++ src/main/java/com/example/todoapp/controller/MvcConfiguration.java
@@ -1,14 +1,23 @@
 package com.example.todoapp.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Set;
 
 // Najpierw są Filtry (LoggerFilter), później Intrrceptory (LoggerInterceptor)
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+    private Set<HandlerInterceptor> interceptors;
+
+    public MvcConfiguration(final Set<HandlerInterceptor> interceptors) {
+        this.interceptors = interceptors;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new LoggerInterceptor());
+        interceptors.forEach(registry::addInterceptor);
     }
 }
