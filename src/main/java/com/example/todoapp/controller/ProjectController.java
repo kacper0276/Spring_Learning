6f8f117d9a64cf8ae894b710156d1e0a1d2b4e6a
kacper0276 <kacper0276@op.pl>
2024-04-20 01:16:29 +0200

    @DateTimeFormat - w jakim formacie mamy otrzymać zmienną, Tworzenie grupy z projektu - kontroler

--- src/main/java/com/example/todoapp/controller/ProjectController.java
+++ src/main/java/com/example/todoapp/controller/ProjectController.java
@@ -5,14 +5,13 @@ import com.example.todoapp.model.Project;
 import com.example.todoapp.model.ProjectStep;
 import com.example.todoapp.model.projection.ProjectWriteModel;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -45,6 +44,17 @@ public class ProjectController {
     @PostMapping(params = "addStep")
     String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
         current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(@ModelAttribute("project") ProjectWriteModel current, Model model, @PathVariable int id, @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline) {
+        try {
+            projectService.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę");
+        } catch (IllegalArgumentException | IllegalStateException ex) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy");
+        }
         return "projects";
     }
 
