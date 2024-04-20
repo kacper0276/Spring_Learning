4bfa89115b8972f3f2a67377c143f8b90aba5b36
kacper0276 <kacper0276@op.pl>
2024-04-20 15:07:59 +0200

    Dodanie groups.html oraz edycja TaskGroupController

--- src/main/java/com/example/todoapp/model/projection/GroupTaskWriteModel.java
+++ src/main/java/com/example/todoapp/model/projection/GroupTaskWriteModel.java
@@ -3,12 +3,14 @@ package com.example.todoapp.model.projection;
 import com.example.todoapp.model.Task;
 import com.example.todoapp.model.TaskGroup;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class GroupTaskWriteModel {
     private String description;
     private LocalDateTime deadline;
