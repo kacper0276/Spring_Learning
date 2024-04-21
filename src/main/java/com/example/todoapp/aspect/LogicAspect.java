88fb6043eb831b343fa46684f9d209ac3f275cf5
kacper0276 <kacper0276@op.pl>
2024-04-22 00:35:15 +0200

    @Before

--- src/main/java/com/example/todoapp/aspect/LogicAspect.java
+++ src/main/java/com/example/todoapp/aspect/LogicAspect.java
@@ -2,9 +2,11 @@ package com.example.todoapp.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,11 @@ public class LogicAspect {
     // MeterRegistry - rejestr metryk
     public LogicAspect(final MeterRegistry registry) {
         projectCreateGroupTimer = registry.timer("logic.product.create.group");
+    }
+
+    @Before("execution(* com.example.todoapp.logic.ProjectService.createGroup(..))")
+    void logMethodCall(JoinPoint joinPoint) {
+        logger.info("Before {} with {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 
     @Around("execution(* com.example.todoapp.logic.ProjectService.createGroup(..))")
