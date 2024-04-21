e3029dfe551a8f929ca29183654840fbee7b152f
kacper0276 <kacper0276@op.pl>
2024-04-22 00:28:44 +0200

    JoinPoint, ProceedingJoinPoint

--- src/main/java/com/example/todoapp/aspect/LogicAspect.java
+++ src/main/java/com/example/todoapp/aspect/LogicAspect.java
@@ -5,11 +5,14 @@ import io.micrometer.core.instrument.Timer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogicAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LogicAspect.class);
     private final Timer projectCreateGroupTimer;
 
     // MeterRegistry - rejestr metryk
