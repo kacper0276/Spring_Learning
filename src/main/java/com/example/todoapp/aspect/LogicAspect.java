1e7791ec644a62ed273be872ebd28741dfb526e0
kacper0276 <kacper0276@op.pl>
2024-04-21 23:51:59 +0200

    Metoda obsługująca @Around advice

--- src/main/java/com/example/todoapp/aspect/LogicAspect.java
+++ src/main/java/com/example/todoapp/aspect/LogicAspect.java
@@ -1,9 +1,15 @@
 package com.example.todoapp.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogicAspect {
+    @Around()
+    Object aroundProjectCreateGroup(ProceedingJoinPoint jp) { // ProceedingJoinPoint - punkt łączenia apsektu z logiką
+        return jp.proceed();
+    }
 }
