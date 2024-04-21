3294e870e55fc9423219eab66967b3a7f81701be
kacper0276 <kacper0276@op.pl>
2024-04-22 00:11:29 +0200

    @Around advice - implementacja metryki

--- src/main/java/com/example/todoapp/aspect/LogicAspect.java
+++ src/main/java/com/example/todoapp/aspect/LogicAspect.java
@@ -1,5 +1,7 @@
 package com.example.todoapp.aspect;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,8 +10,24 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LogicAspect {
+    private final Timer projectCreateGroupTimer;
+
+    // MeterRegistry - rejestr metryk
+    public LogicAspect(final MeterRegistry registry) {
+        projectCreateGroupTimer = registry.timer("logic.product.create.group");
+    }
+
     @Around("execution(* com.example.todoapp.logic.ProjectService.createGroup(..))")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp) { // ProceedingJoinPoint - punkt łączenia apsektu z logiką
-        return jp.proceed();
+        return projectCreateGroupTimer.record(() -> {
+            try {
+                return jp.proceed();
+            } catch (Throwable e) {
+                if (e instanceof RuntimeException) {
+                    throw (RuntimeException) e;
+                }
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
