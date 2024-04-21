59aefd4494d0704f6d89f96463e4ece61932629f
kacper0276 <kacper0276@op.pl>
2024-04-21 09:53:56 +0200

    @Aspect

---
+++ src/main/java/com/example/todoapp/aspect/LogicAspect.java
@@ -0,0 +1,9 @@
+package com.example.todoapp.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogicAspect {
+}
