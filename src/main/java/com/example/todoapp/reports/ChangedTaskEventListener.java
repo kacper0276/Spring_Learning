8c276ec0d4f388b99450daadc52ad6bdc45a1c75
kacper0276 <kacper0276@op.pl>
2024-04-23 16:49:28 +0200

    Log zdarzeń

--- src/main/java/com/example/todoapp/reports/ChangedTaskEventListener.java
+++ src/main/java/com/example/todoapp/reports/ChangedTaskEventListener.java
@@ -1,6 +1,7 @@
 package com.example.todoapp.reports;
 
 import com.example.todoapp.model.event.TaskDone;
+import com.example.todoapp.model.event.TaskEvent;
 import com.example.todoapp.model.event.TaskUndone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,24 @@ import org.springframework.stereotype.Service;
 public class ChangedTaskEventListener {
     private static final Logger logger = LoggerFactory.getLogger(ChangedTaskEventListener.class);
 
+    private final PersistedTaskEventRepository repository;
+
+    public ChangedTaskEventListener(PersistedTaskEventRepository repository) {
+        this.repository = repository;
+    }
+
     @EventListener
     public void on(TaskDone event) {
-        logger.info("Got " + event);
+        onChanged(event);
     }
 
     @EventListener
     public void on(TaskUndone event) {
+        onChanged(event);
+    }
+
+    private void onChanged(final TaskEvent event) {
         logger.info("Got " + event);
+        repository.save(new PersistedTaskEvent(event));
     }
 }
