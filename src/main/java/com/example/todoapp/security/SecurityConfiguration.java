1948eb38300329386fed755ba5880e90bce5a3dc
kacper0276 <kacper0276@op.pl>
2024-04-24 13:35:48 +0200

    Działająca wersja Keycloak

--- src/main/java/com/example/todoapp/security/SecurityConfiguration.java
+++ src/main/java/com/example/todoapp/security/SecurityConfiguration.java
@@ -1,30 +1,77 @@
 package com.example.todoapp.security;
 
-import lombok.RequiredArgsConstructor;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
-public class SecurityConfiguration {
-    @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf(crsf -> crsf.disable());
+class SecurityConfiguration {
 
-        http.authorizeHttpRequests(
-                authorize ->
-                        authorize.requestMatchers("/info/*").hasRole("USER")
-                                .anyRequest().permitAll()
-        );
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri(URI.create("http://localhost:8080").toString());
+        return successHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorise ->
+                authorise
+                        .requestMatchers("/info/*").hasRole("user")
+                        .anyRequest().permitAll());
+
+        http.oauth2Login(withDefaults())
+                .logout(logout ->
+                        logout.logoutSuccessHandler(oidcLogoutSuccessHandler));
 
         return http.build();
     }
 
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority oidcUserAuthority) {
+                    OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+                    Map<String, Object> realmAccess = userInfo.getClaim("realm_access");
+                    Collection<String> realmRoles;
+                    if (realmAccess != null
+                            && (realmRoles = (Collection<String>) realmAccess.get("roles")) != null) {
+                        realmRoles
+                                .forEach(role -> mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+                    }
+                }
+            });
+
+            return mappedAuthorities;
+        };
+    }
 }
